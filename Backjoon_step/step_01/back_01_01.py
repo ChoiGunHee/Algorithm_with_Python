4305c2482261ca33a5a9061c4638d1d59311ae12
GunHee Choi <godrjsmgl@gmail.com>
2021-02-22 08:33:21 +0000

    Solve Backjoon step1 01_01, 01_02 with Python

--- Backjoon_step/step_01/back_01_01.py
+++ Backjoon_step/step_01/back_01_01.py
@@ -1,1 +1,12 @@
+'''
+#2021. 02. 22
+#Creater : Gunhee Choi
+#Problem Number : 2557
+#Title : Hello World
+
+#Problem : Hello World!를 출력하시오.
+#Input : None
+#Output : Hello World!
+'''
+
 print('Hello World!')
---
+++ Backjoon_step/step_01/back_01_02.py
@@ -0,0 +1,15 @@
+'''
+#2021. 02. 22
+#Creater : Gunhee Choi
+#Problem Number : 2557
+#Title : Hello World
+
+#Problem : ACM-ICPC 인터넷 예선, Regional, 그리고 World Finals까지 이미 2회씩 진출해버린 kriii는 미련을 버리지 못하고 왠지 모르게 올해에도 파주 World Finals 준비 캠프에 참여했다.
+대회를 뜰 줄 모르는 지박령 kriii를 위해서 격려의 문구를 출력해주자.
+
+#Input : 본 문제는 입력이 없다.
+#Output : 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+'''
+
+print('강한친구 대한육군')
+print('강한친구 대한육군')
