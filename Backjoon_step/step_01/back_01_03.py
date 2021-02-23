456a882070029d454113c6384fb71b6d10cbba4c
GunHee Choi <godrjsmgl@gmail.com>
2021-02-23 08:43:25 +0000

    Solve Backjoon Step1 01_03

---
+++ Backjoon_step/step_01/back_01_03.py
@@ -0,0 +1,15 @@
+'''
+#2021. 02. 23
+#Creater : Gunhee Choi
+#Problem Number : 2557
+#Title : 고양이
+
+#Problem : 아래 예제와 같이 고양이를 출력하시오.
+#Input : None
+#Output : 고양이를 출력한다.
+'''
+
+print('''\\    /\\''')
+print(''' )  ( ')''')
+print('''(  /  )''')
+print(''' \\(__)|''')
