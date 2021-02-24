ba643672cc999231f18fbbc0c4f2fa2a05ac9391
GunHee Choi <godrjsmgl@gmail.com>
2021-02-24 08:31:39 +0000

    Solve Backjoon01 01_04

---
+++ Backjoon_step/step_01/back_01_04.py
@@ -0,0 +1,17 @@
+'''
+#2021. 02. 24
+#Creater : Gunhee Choi
+#Problem Number : 10172
+#Title : 개
+
+#Problem : 아래 예제와 같이 개를 출력하시오.
+#Input : None
+#Output : 개를 출력한다.
+'''
+
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\__|")
+
