160c3f554585d4b72dadb5c0eab9bf07282312ff
GunHee Choi <godrjsmgl@gmail.com>
2021-03-17 10:33:03 +0000

    Study Python Bool

---
+++ JumpToPython/01_DataType/07_Bool.py
@@ -0,0 +1,31 @@
+'''
+#2021. 03. 17
+#Creater : Gunhee Choi
+#Link : https://wikidocs.net/book/1
+#Title : Bool 자료형
+'''
+
+#Bool 자료형
+>>> a = True
+>>> b = False
+
+>>> type(a)
+<class 'bool'>
+>>> type(b)
+<class 'bool'>
+
+
+>>> bool('python')
+True
+
+>>> bool('')
+False
+
+>>> bool([1,2,3])
+True
+>>> bool([])
+False
+>>> bool(0)
+False
+>>> bool(3)
+True
