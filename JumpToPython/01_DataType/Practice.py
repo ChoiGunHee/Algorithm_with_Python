b469d72f26c713fe8dad55480db78f563de958f9
GunHee Choi <godrjsmgl@gmail.com>
2021-03-17 10:45:49 +0000

    Create Practice.py -> DataType

---
+++ JumpToPython/01_DataType/Practice.py
@@ -0,0 +1,16 @@
+'''
+#2021. 03. 17
+#Creater : Gunhee Choi
+#Link : https://wikidocs.net/book/1
+#Title : 연습문제 (데이터 타입)
+'''
+
+#Q1.
+a = 80
+b = 75
+c = 55
+avg = (a + b + c)/3
+
+#Q2.
+#% 연산자 이용
+
