67664c1e7b25f6f3df270ffc8dc829ac6feba184
GunHee Choi <godrjsmgl@gmail.com>
2021-03-23 09:59:11 +0000

    Solve Backjoon Step01 01_06

---
+++ Backjoon_step/step_01/back_01_06.py
@@ -0,0 +1,17 @@
+'''
+#2021. 03. 23
+#Creater : Gunhee Choi
+#Problem Number : 1001
+#Title : A-B
+
+#Problem : 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+#Input : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+    3 2
+
+#Output : 첫째 줄에 A-B를 출력한다.
+    1
+'''
+
+a, b = map(int, input().split())
+print(a-b)
