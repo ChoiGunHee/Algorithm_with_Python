04e0f6282fca41b56999fe23c7d9135026e6d67d
GunHee Choi <godrjsmgl@gmail.com>
2021-03-30 11:01:14 +0000

    Study Python in_output

---
+++ JumpToPython/03_Function/02_In_and_out.py
@@ -0,0 +1,28 @@
+'''
+#2021. 03. 30
+#Creater : Gunhee Choi
+#Link : https://wikidocs.net/book/1
+#Title : 입출력
+'''
+
+#input
+>>> a = input()
+Life is too short, you need python
+>>> a
+'Life is too short, you need python'
+
+>>> number = input("숫자를 입력하세요: ")
+숫자를 입력하세요: 3
+>>> print(number)
+3
+
+#output
+>>> a = 123
+>>> print(a)
+123
+>>> a = "Python"
+>>> print(a)
+Python
+>>> a = [1, 2, 3]
+>>> print(a)
+[1, 2, 3]
