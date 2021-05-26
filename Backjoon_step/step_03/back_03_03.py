52843813650faa81f06620c5b5dc09ea02a7f446
GunHee Choi <godrjsmgl@gmail.com>
2021-05-26 22:05:19 +0000

    Create Problem 8393

---
+++ Backjoon_step/step_03/back_03_03.py
@@ -0,0 +1,19 @@
+'''
+* 2021. 05. 26
+* Creater : Gunhee Choi
+* Problem Number : 8393
+* Title : 합
+* Problem :
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+* Input :
+첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+    3
+	
+* Output :
+1부터 n까지 합을 출력한다.
+
+    6
+    
+'''
